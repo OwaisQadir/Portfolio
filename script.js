a73678c25cdc7a3c71021665ff7f030a1098d510
OwaisQadir <158231961+OwaisQadir@users.noreply.github.com>
2026-03-29 17:21:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
-const text = ["Data Analyst", "Dashboard Expert", "Web Scraper"];
-let i=0,j=0,current="",isDeleting=false;
+document.querySelectorAll(".gallery").forEach((gallery) => {
+  let index = 0;
+  const images = gallery.querySelectorAll("img");
 
-function type(){
-  current=text[i];
-  document.getElementById("typing").textContent=current.substring(0,j);
-
-  if(!isDeleting && j<current.length) j++;
-  else if(isDeleting && j>0) j--;
-  else{
-    isDeleting=!isDeleting;
-    if(!isDeleting) i=(i+1)%text.length;
+  function show(i) {
+    images.forEach(img => img.classList.remove("active"));
+    images[i].classList.add("active");
   }
 
-  setTimeout(type,isDeleting?50:100);
-}
-type();
+  gallery.querySelector(".next").onclick = () => {
+    index = (index + 1) % images.length;
+    show(index);
+  };
 
-let index = 0;
-const images = document.querySelectorAll(".gallery img");
+  gallery.querySelector(".prev").onclick = () => {
+    index = (index - 1 + images.length) % images.length;
+    show(index);
+  };
 
-function show(i){
-  images.forEach(img => img.classList.remove("active"));
-  images[i].classList.add("active");
-}
-
-function next(){
-  index = (index + 1) % images.length;
   show(index);
-}
-
-function prev(){
-  index = (index - 1 + images.length) % images.length;
-  show(index);
-}
-
-show(index);
+});
